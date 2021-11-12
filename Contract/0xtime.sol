e0fc785cb70549c0e1612c8cb0c1b7d18d54a86d
antiyro <bouhadananathan@gmail.com>
2021-11-12 19:04:30 +0100

    Smart contract

--- Contract/0xtime.sol
+++ Contract/0xtime.sol
@@ -4,17 +4,12 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Time is ERC721, Ownable, ERC721Enumerable {
+contract Time is ERC721, Ownable {
     
     address private owners;
     
     //MINTING PARAMETERS
-    //Variables for token enumeration
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
-    //Minting variables
     uint256 public currentId = 0;
     bool public minted = false;
     
@@ -22,19 +17,16 @@ contract Time is ERC721, Ownable, ERC721Enumerable {
     uint public auctionEndTime;
     uint public STARTING_PRICE = 1000000000000000;
     uint public bank = 0;
-    
-    //current state of the auction
     address public highestBidder;
     uint public highestBid;
+    address public pastWinner;
     bool started = false;
     
-    mapping(address => uint) public pendingReturns; //Withdrawable amouts of outbided address
+    mapping(address => uint) public pendingReturns; //Withdrawable amounts of outbided address
     mapping(address => uint) public comunityBalance; //Recording comunity balance for rewards
 
     uint public comunityReward;
     uint private devRewards;
-    
-    event HighestBidIncrease(address bidder, uint amout);
     
     constructor() ERC721("Time", "0xTime") {
         owners = msg.sender;
@@ -43,8 +35,8 @@ contract Time is ERC721, Ownable, ERC721Enumerable {
     //AUCTION SYSTEM
     function bid() public payable {
         if (block.timestamp > auctionEndTime) {
-            comunityReward = highestBid / 2 / totalSupply();
-            for (uint i = 0; i < totalSupply(); i++)
+            comunityReward = highestBid / 2 / currentId;
+            for (uint i = 0; i < currentId; i++)
                     comunityBalance[ownerOf(i)] += comunityReward;
             devRewards += highestBid / 2;
             if (!minted)
@@ -56,6 +48,7 @@ contract Time is ERC721, Ownable, ERC721Enumerable {
         if (started == false) {
             auctionEndTime = block.timestamp + (86400 - (block.timestamp % 86400));
             highestBid = STARTING_PRICE;
+            pastWinner = highestBidder;
             started == true;
         }
         require(block.timestamp < auctionEndTime, "The auction has already ended");
@@ -65,7 +58,6 @@ contract Time is ERC721, Ownable, ERC721Enumerable {
         }
         highestBidder = msg.sender;
         highestBid = msg.value;
-        emit HighestBidIncrease(msg.sender, msg.value);
     }
     
     function withdraw() public returns(bool) {
@@ -88,19 +80,19 @@ contract Time is ERC721, Ownable, ERC721Enumerable {
 
 
     //MINTING SYSTEM
-    // The 2 following functions are overrides required by Solidity.
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
-        return super.supportsInterface(interfaceId);
-    }
-    
     //function to mint the day NFT, should be onlyWinner
     function safeMint(address to) public  {
-        _safeMint(to, currentId);
-        currentId++;
-        minted = true;
+        if (block.timestamp > auctionEndTime) {
+            require(to == highestBidder, "You are not the winner of the auction.");
+            _safeMint(to, currentId);
+            currentId++;
+            minted = true;
+        }
+        else if (pastWinner != address(0)) {
+            require(to == pastWinner, "You are not the winner of the auction.");
+            _safeMint(to, currentId);
+            currentId++;
+            minted = true;
+        }
     }
 }
