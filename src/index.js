d8fd7c322ef0925bb9291bc3b549bc1a1ff5ebca
root <root@sweet-lumiere.87-106-196-179.plesk.page>
2021-11-16 18:45:09 +0000

    commit4commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -19,4 +20,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
+
