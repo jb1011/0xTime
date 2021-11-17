8338cbcca18fbb1c264822076b8033d3b1e22016
jb1011 <demenet.jb@hotmail.fr>
2021-11-17 12:32:44 +0100

    new serv.js

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-const http = require('http');
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const server = express();
+const express = require('express')
 
-server.use(bodyParser.urlencoded({extended: false}));
-server.use(bodyParser.json());
 
-server.get('/', function(req, res, next){
-    res.sendFile(path.join(__dirname+ '/public/index.html'));
-});
+const path = require('path')
+//pour pouvoir mettre des variable d'environnement dans le .env
+require('dotenv').config()
 
-server.listen(3000,()=>{
-    console.log('server is running')
-});
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+app.use(express.static('client/build'))
+
+app.get('/api/youtube', (req, res) => {
+    res.send({
+        msg: 'Salut World'
+    })
+})
+
+app.get('/*', (_, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
+
+//on dit à l'app de se lancer sur le port https://5000
+app.listen(PORT, () => {
+    console.log(`Le serveur est lancé sur le port ! ${PORT}`)
+})
