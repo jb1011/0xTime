9f4214c1c1112fced2374249b5fa089429e9b148
jb1011 <demenet.jb@hotmail.fr>
2021-11-27 15:27:41 +0100

    dynamic ok

--- client/src/components/Dates.js
+++ client/src/components/Dates.js
@@ -1,10 +1,20 @@
-let date=[
-    {date: '12 novembre 2021', id:1},
-    {date: '13 novembre 2021', id:2},
-    {date: '14 novembre 2021', id:3},
-    {date: '15 novembre 2021', id:4},
+// let date=[
+//     {date: '12 novembre 2021', id:1},
+//     {date: '13 novembre 2021', id:2},
+//     {date: '14 novembre 2021', id:3},
+//     {date: '15 novembre 2021', id:4},
+
+// ]
+
+let events=[
+    {event: '26 novembre 2021', name: 'Damien', id: '2510'},
+    {event: '27 novembre 2021', name: 'Robin', id: '2610'},
+    {event: '28 novembre 2021', name: 'Alain', id: '2710'},
+    {event: '29 novembre 2021', name: 'Gerard', id: '2810'},
 
 ]
 
-export { date };
+
+
+export { events };
 
--- client/src/pages/MyCalendar.js
+++ client/src/pages/MyCalendar.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Nav from '../components/Nav'
 import Calendar from 'react-calendar'
 import CardPres from '../components/CardPres'
 import { BrowserRouter as Router,Link,Route } from 'react-router-dom'
-import { date } from '../components/Dates'
+import { date, events } from '../components/Dates'
 import Footer from '../components/Footer'
 
 function MyCalendar() {
     
+    const[date, setDate] = useState(new Date());
+    const[data, setData] = useState();
 
+    const onChange = date => {
+        // console.log(date)
+        let pick = date.getUTCDate().toString() + date.getMonth().toString();
+        let data = events.find(item => item.id === pick)
+        setData(data);
+        setDate(date);
+    };
+    
+
+    
     return (
         <div className="container">
             <Nav />
             <div class="content row pt-5">
-            <Router>
+                <div class="col-sm pop-up-card">
+                    <h1>Owner :</h1>
+                    <h2>{data && data.name}</h2>
+                    <h1>Date :</h1>
+                    <h2>{data && data.event}</h2>
+
+                
+
+                </div>
+                <div class="col-sm">
+                    <Calendar className="react-calendar" onChange={onChange} value={date} />
+                </div>
+                    {/* <Router>
                     <div>
                         <h1>dynamic router</h1>
                         {
@@ -25,12 +49,7 @@ function MyCalendar() {
                         <Route path="/MyCalendar/date/:id/:date"><CardPres /></Route>
                     </div>
                     </Router>
-                
-                    <div class="col-sm">
-                        <Calendar className="react-calendar"/>
-
-                    </div>
-                    
+                 */}
 
             </div>
             <Footer />
